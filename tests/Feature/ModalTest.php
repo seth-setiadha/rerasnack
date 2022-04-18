2157312a0d4dd3b52740c6e9a0aed070736d3187
Seth Lab <seth.setiadha@gmail.com>
2022-04-18 14:39:11 -0400

    update modal test

--- tests/Feature/ModalTest.php
+++ tests/Feature/ModalTest.php
@@ -28,16 +28,14 @@ class ModalTest extends TestCase
      *
      * @return void
      */
-    public function test_user_create_item()
-    {
+    public function test_user_create_modal()
+    {       
         $this->signIn();
-        $attributes = [
-            'item_code' => $this->faker->regexify('[A-Z]{1}[0-4]{3}'),
-            'item_name' => $this->faker->name(),
-            'bal_kg' => $this->faker->randomFloat(1, 0.5, 10),
-        ];
 
-        $response = $this->create($attributes);
+        $response = $this->get('/modal');
+        $response->assertStatus(200);
+        $response->assertSeeText('Modal');
+        $response->assertSeeText('Tambah Modal');
     }
 
     /**
@@ -45,33 +43,10 @@ class ModalTest extends TestCase
      *
      * @return void
      */
-    public function test_unauthenticated_user_can_not_create_item()
+    public function test_unauthenticated_user_can_not_create_modal()
     {
-        $attributes = [
-            'item_code' => $this->faker->regexify('[A-Z]{1}[0-4]{3}'),
-            'item_name' => $this->faker->name(),
-            'bal_kg' => $this->faker->randomFloat(1, 0.5, 10),
-        ];
-
-        $response = $this->failCreate($attributes);
-
+        $response = $this->get('/modal');
         $response->assertRedirect('/login');
     }
 
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_user_update_item()
-    {
-        $this->signIn();
-        $attributes = [
-            'item_code' => $this->faker->regexify('[A-Z]{1}[0-4]{3}'),
-            'item_name' => $this->faker->name(),
-            'bal_kg' => $this->faker->randomFloat(1, 0.5, 10),
-        ];
-
-        $response = $this->update($attributes);
-    }
 }
